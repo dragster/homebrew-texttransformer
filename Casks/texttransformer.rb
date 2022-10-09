86af4f0abc146777d503036b64977eee4de95b21
D <>
2022-10-09 16:06:24 +0900

    Cask

---
+++ Casks/texttransformer.rb
@@ -0,0 +1,11 @@
+cask "texttransformer" do
+  version "0.1"
+  sha256 :no_check
+
+  url "https://www.dragster.dev/texttransformer/texttransformer-#{version}.dmg"
+  name "Text Transformer"
+  desc "Transform text software"
+  homepage "https://www.dragster.dev/texttransformer/"
+
+  app "Text Transformer.app"
+end
